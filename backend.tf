cbdceff9146c010e840f1123c55ed97eeddbbb08
Ola Lawal <86223667+galaxyenergy@users.noreply.github.com>
2022-06-10 22:38:37 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "ola-tf-state-bucket"
     key = "main"
-    region = "us-east-2"
+    region = "us-east-1"
     dynamodb_table = "ola-dynamodb-table"
   }
 }
